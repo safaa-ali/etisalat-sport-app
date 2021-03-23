009ffb3d6a957570762790f7d49a5c5f4f9e34e4
safaa ali <safaaaly246@gmail.com>
2021-03-23 02:48:04 +0200

    add firebase but network is missing

--- js/main.js
+++ js/main.js
@@ -3,6 +3,20 @@
 
 
 
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDz8gtwLHf939Rj9XzXGz6yV8o3B36GaqI",
+    authDomain: "etisalat-app1.firebaseapp.com",
+    projectId: "etisalat-app1",
+    storageBucket: "etisalat-app1.appspot.com",
+    messagingSenderId: "629452538227",
+    appId: "1:629452538227:web:176d7c0777cbfa4f600506",
+    measurementId: "G-W3ZR8C0PE6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 // WOW ANIMATION
 new WOW({ mobile: false }).init();
 
